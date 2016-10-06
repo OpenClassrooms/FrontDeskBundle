6078c0bca8c0c84db0d369c0886c6b995ecf3f3b
Killian Herbunot <killian.herbunot@openclassrooms.com>
2016-10-06 16:57:00 +0200

    Create FrontDeskBundle

---
+++ DependencyInjection/OpenClassroomsFrontDeskExtension.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace OpenClassrooms\Bundle\FrontDeskBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+/**
+ * @author Killian Herbunot <killian.herbunot@openclassrooms.com>
+ */
+class OpenClassroomsFrontDeskExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $config, ContainerBuilder $container)
+    {
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config/'));
+        $loader->load('services.xml');
+
+        $config = $this->processConfiguration(new Configuration(), $config);
+
+        $container->setParameter('openclassrooms.frontdesk.key', $config['key']);
+        $container->setParameter('openclassrooms.frontdesk.token', $config['token']);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAlias()
+    {
+        return 'openclassrooms_frontdesk';
+    }
+}
---
+++ Tests/DependencyInjection/OpenClassroomsFrontDeskExtensionTest.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace OpenClassrooms\Bundle\FrontDeskBundle\Tests\DependencyInjection;
+
+use OpenClassrooms\Bundle\FrontDeskBundle\DependencyInjection\OpenClassroomsFrontDeskExtension;
+use OpenClassrooms\Bundle\FrontDeskBundle\OpenClassroomsFrontDeskBundle;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+/**
+ * @author Killian Herbunot <killian.herbunot@openclassrooms.com>
+ */
+class OpenClassroomsFrontDeskExtensionTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var YamlFileLoader
+     */
+    protected $configLoader;
+
+    /**
+     * @var ContainerBuilder
+     */
+    protected $container;
+
+    /**
+     * @var ExtensionInterface
+     */
+    protected $extension;
+
+    /**
+     * @var XmlFileLoader
+     */
+    protected $serviceLoader;
+
+    /**
+     * @test
+     * @expectedException \Symfony\Component\Config\Definition\Exception\InvalidConfigurationException
+     */
+    public function NoConfiguration_ThrowException()
+    {
+        $this->configLoader->load('empty_config.yml');
+        $this->container->compile();
+    }
+
+    /**
+     * @test
+     * @expectedException \Symfony\Component\Config\Definition\Exception\InvalidConfigurationException
+     */
+    public function WithoutTokenConfiguration_ThrowException()
+    {
+        $this->configLoader->load('without_token_config.yml');
+        $this->container->compile();
+    }
+
+    /**
+     * @test
+     * @expectedException \Symfony\Component\Config\Definition\Exception\InvalidConfigurationException
+     */
+    public function WithoutApiKeyConfiguration_ThrowException()
+    {
+        $this->configLoader->load('without_api_key_config.yml');
+        $this->container->compile();
+    }
+
+    /**
+     * @test
+     */
+    public function PersonServiceReturnPerson()
+    {
+        $this->configLoader->load('config.yml');
+        $this->container->compile();
+        $personService = $this->container->get('openclassrooms.frontdesk.service.person');
+        $this->assertInstanceOf('OpenClassrooms\FrontDesk\Services\Impl\PersonServiceImpl', $personService);
+    }
+
+    /**
+     * @test
+     */
+    public function PackServiceReturnPack()
+    {
+        $this->configLoader->load('config.yml');
+        $this->container->compile();
+        $packService = $this->container->get('openclassrooms.frontdesk.service.pack');
+        $this->assertInstanceOf('OpenClassrooms\FrontDesk\Services\Impl\PackServiceImpl', $packService);
+    }
+
+    protected function setUp()
+    {
+        $this->container = new ContainerBuilder();
+        $this->extension = new OpenClassroomsFrontDeskExtension();
+        $this->container->set('request_stack', new RequestStack());
+        $this->container->registerExtension($this->extension);
+        $this->container->loadFromExtension('openclassrooms_frontdesk');
+
+        $this->configLoader = new YamlFileLoader(
+            $this->container,
+            new FileLocator(__DIR__.'/Fixtures/Resources/config')
+        );
+
+        $this->serviceLoader = new XmlFileLoader(
+            $this->container,
+            new FileLocator(__DIR__.'/../../Resources/config')
+        );
+
+        $this->serviceLoader->load('services.xml');
+
+        $bundle = new OpenClassroomsFrontDeskBundle();
+        $bundle->build($this->container);
+    }
+}
